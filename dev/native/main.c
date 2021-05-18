7dfff1ba6791a0c647fcd7634cfd4622423e5346
Federico Paglioni <fedepaj@enzomanjaro.lan>
2021-05-18 09:42:14 +0200

    Add fws

--- dev/native/main.c
+++ dev/native/main.c
@@ -4,15 +4,15 @@
 #include <math.h>
 #include "xtimer.h"
 #include "fmt.h"
-
-#include "periph/gpio.h"
-#include "srf04.h"
-#include "srf04_params.h"
+#include <timex.h>
 
 //Networking
 #include "msg.h"
 #include "net/emcute.h"
 #include "net/ipv6/addr.h"
+
+//Shell
+#include "shell.h"
 
 //Networking
 #define _IPV6_DEFAULT_PREFIX_LEN        (64U)
@@ -25,50 +25,13 @@
 static emcute_sub_t subscriptions[NUMOFSUBS];
 static char stack[THREAD_STACKSIZE_MAIN];
 
-//Dev fast forward
-#if (FF!=0)
-#define SLEEP 1 //If FF is set to 1 the device will sllep 1 sec between measurements
-#else
-#define SLEEP 60*10 //Nominal 10 min sleep beween measurements
-#endif
-
-//Samples to take each time we sense
-#define SAMPLES 10
-//No of ultrasonic sensor used
-#define SENSORS 2
-//When to send data
-#define ROUNDS 10
-//Undefine to make parallel mesurements (experimenting)
-#define SERIAL 1
-
-#define LOW_PERCENTILE 0.25
-#define HIGH_PERCENTILE 0.75
-
-//Structure definition to store ref values
-typedef struct refer{
-    int low;
-    int high;
-} refer;
-
-//State machine states enumeration
-typedef enum state {
-    STANDBY, REFERENCE, SAMPLING, CLEAN, SEND
-}state;
-
-//Current state
-state curr_state = REFERENCE;
-
-
-gpio_t trigger[] = {GPIO_PIN(PORT_A, 10)/*D2*/, GPIO_PIN(PORT_B, 3)/*D3*/};//, GPIO_PIN(PORT_B, 5)/*D4*/};
-gpio_t echo[] = {GPIO_PIN(PORT_B, 4)/*D5*/, GPIO_PIN(PORT_B, 10)/*D6*/};//,GPIO_PIN(PORT_A, 8)/*D7*/};
-srf04_t dev[SENSORS];
-
+#define SLEEP 10 // Time in mins
 
 static void on_pub(const emcute_topic_t *topic, void *data, size_t len){
     char *in = (char *)data;
     printf("### got publication for topic '%s' [%i] ###\n",
            topic->name, (int)topic->id);
-    //Parse input and set led
+    //Print the input
     for (size_t i = 0; i < len; i++) {
         printf("%c", in[i]);
     }
@@ -82,6 +45,7 @@ static void *emcute_thread(void *arg)
     emcute_run(CONFIG_EMCUTE_DEFAULT_PORT, EMCUTE_ID);
     return NULL;    /* should never be reached */
 }
+
 
 /********* NETWORK RELATED PART *********/ 
 static uint8_t get_prefix_len(char *addr)
@@ -155,7 +119,7 @@ int setup_mqtt(void)
     /* start the emcute thread */
     thread_create(stack, sizeof(stack), EMCUTE_PRIO, 0, emcute_thread, NULL, "emcute");
     //Adding address to network interface
-    netif_add("4","fec0:affe::99");
+    netif_add("5","fec0:affe::99");
     // connect to MQTT-SN broker
     printf("Connecting to MQTT-SN broker %s port %d.\n", SERVER_ADDR, SERVER_PORT);
 
@@ -224,83 +188,6 @@ static int pub(char* topic, const char* data, int qos){
     return 0;
 }
 
-void init(void){
-    srf04_params_t my_params[SENSORS];
-    for(int i=0;i<SENSORS;i++){
-        my_params[i].trigger = trigger[i];
-        my_params[i].echo = echo[i];
-    }
-    for(int i=0;i<SENSORS;i++){
-        if(srf04_init(&dev[i], &my_params[i]) != SRF04_OK){
-            puts("Failed to setup SRF04");
-        }
-    }
-    setup_mqtt();
-}
-
-int compare( const void *arg1, const void *arg2 )
-{
-    return arg1>arg2;
-}
-
-#ifndef SERIAL
-void measure(int measures[SENSORS][SAMPLES]){
-    puts("STARTING MEASUREMENTS");
-    //Reads sensors values
-    for(int j=0;j<SAMPLES;j++){
-        for(int i=0;i<SENSORS;i++){
-            srf04_trigger(&dev[i]);
-        }
-        xtimer_msleep(300);
-        for(int i=0;i<SENSORS;i++){
-            measures[i][j]=srf04_read(&dev[i]);
-            printf("%d, ", measures[i][j]);
-        }
-        printf("\n");
-        xtimer_sleep(2);
-    }
-}
-#else
-void measure(int measures[SENSORS][SAMPLES]){
-    for(int j=0;j<SAMPLES;j++){
-        for(int i=0;i<SENSORS;i++){
-            srf04_trigger(&dev[i]);
-            xtimer_msleep(300);
-            measures[i][j]=srf04_read(&dev[i]);
-            printf("%d, ", measures[i][j]);
-            xtimer_sleep(2);
-        }
-        printf("\n");
-    }
-}
-#endif
-
-void reference(refer *refer){
-    int measures[SENSORS][SAMPLES];
-    measure(measures);
-    for(int i=0;i<SENSORS;i++){
-        qsort(measures[i],SAMPLES,sizeof(int),compare);
-        refer[i].low=measures[i][(int)ceil(LOW_PERCENTILE*SAMPLES)];
-        refer[i].high=measures[i][(int)ceil(HIGH_PERCENTILE*SAMPLES)];
-    }
-}
-
-/* The sense method now checks if data from the sensor differs from reference stored values.
-** In case it differs it returns that the flow in the pipe is the maximum value.
-** A future implementation should look for the distance of the sensed value from either
-** low and high percentiles to give the real value of flow in the pipe.
-*/
-int sense(refer *refer){
-    int measures[SENSORS][SAMPLES];
-    measure(measures);
-    for(int i=0;i<SENSORS;i++){
-        qsort(measures[i],SAMPLES,sizeof(int),compare);
-        int median = measures[i][(int)ceil(0.5*SAMPLES)];
-        if(median>refer[i].high || median<refer[i].low){
-            return 100;
-        }
-    } return 0;
-}
 
 void send(int val){
     printf("VALUE: %d\n",val);
@@ -309,47 +196,44 @@ void send(int val){
     pub(MQTT_TOPIC,str,0);
 }
 
-int agg_flows(int *flows){
-    qsort(flows,ROUNDS,sizeof(int),compare);
-    return flows[(int)ceil(0.5*ROUNDS)];
+int scheduled(int schedule[], int len){
+    for(int i=0;i<len;i++){
+        xtimer_sleep(SLEEP);
+        send(schedule[i]);
+    }
+    puts("DONE");
+    return 0;
 }
 
-int main(void){
-    refer refer[SENSORS];
-    int flows[ROUNDS];
-    int round=0;
-    init();
-    while(1){
-        switch(curr_state){
-            case REFERENCE:
-                puts("ENTER REFERENCE");
-                reference(refer);
-                puts("EXIT REFERENCE");
-                curr_state=STANDBY;
-                break;
-            case SAMPLING:
-                puts("ENTER SAMPLING");
-                flows[round]=sense(refer);
-                round++;
-                puts("EXIT SAMPLING");
-                if(round<ROUNDS)
-                curr_state=STANDBY;
-                else curr_state=SEND;
-                break;
-            case SEND:
-                send(agg_flows(flows));
-                curr_state=CLEAN;
-                break;
-            case CLEAN:
-                round=0;
-                curr_state=STANDBY;
-                break;
-            case STANDBY:
-                puts("ENTER STANDBY");
-                xtimer_sleep(SLEEP);
-                puts("EXIT STANDBY");
-                curr_state=SAMPLING;
-                break;
-        }
+static int cmd_flow(int argc, char **argv){
+    int schedule[150];
+    if(argc<2){
+        printf("usage: %s <flow value>|[<flow value 1> <hold for mins 1> ... <flow value n><hold for mins n>]\n", argv[0]);
+        return 1;
     }
+    if(argc==2){
+        send(atoi(argv[1]));
+        printf("%s set to %d\n",argv[0],atoi(argv[1]));
+    }
+    if(argc>2){
+        argv++;
+        argc--;
+        for(int i=0;i<argc;i++){
+            schedule[i]=atoi(argv[i]);
+        }
+        scheduled(schedule,argc);
+    }
+    return 0;
+}
+
+static const shell_command_t shell_commands[] = {
+    { "flow", "set the device's current flow", cmd_flow },
+    { NULL, NULL, NULL }};
+
+int main(void){
+    setup_mqtt();
+    /* start shell */
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    return 0;
 }
---
+++ dev/nucleo/flash.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#!!!!!!RUN WITH SUDO!!!!!!
+./../RIOT/dist/tools/tapsetup/tapsetup -d
+./../RIOT/dist/tools/tapsetup/tapsetup
+ip a a fec0:affe::1/64 dev tapbr0
+make BUILD_IN_DOCKER=1 BOARD=nucleo-f401re flash term
